dec829c561b4ccd3fbacf353370192ad0784ab0a
zhangyun <964822844@qq.com>
2020-06-20 00:11:15 +0800

    完成了文章发布功能

---
+++ assets/js/article/art_list.js
@@ -0,0 +1,28 @@
+$(function() {
+    var layer = layui.layer;
+    var form = layui.form;
+    var q = {
+        pagenum: 1,
+        pagesize: 2,
+        cate_id: '',
+        state: ''
+    }
+
+    initTable();
+
+    function initTable() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/list',
+            data: q,
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg("获取文章列表失败！")
+                }
+                // 渲染表格
+                var htmlStr = template("art_table", res);
+                $("#art_lists").html(htmlStr);
+            }
+        })
+    }
+})
---
+++ assets/js/article/art_pub.js
@@ -0,0 +1,86 @@
+$(function() {
+    var layer = layui.layer;
+    var form = layui.form;
+
+    initSelectCate();
+    // 初始化富文本编辑器
+    initEditor()
+        // 1. 初始化图片裁剪器
+    var $image = $('#image')
+
+    // 2. 裁剪选项
+    var options = {
+        aspectRatio: 400 / 280,
+        preview: '.img-preview'
+    }
+
+    // 3. 初始化裁剪区域
+    $image.cropper(options);
+    //更换裁剪区域图片
+    $("#selectFaceBtn").on("click", function(e) {
+        $("#face").click();
+    })
+    $("#face").on("change", function(e) {
+        var file = e.target.files[0];
+        var newImgURL = URL.createObjectURL(file);
+        $image
+            .cropper('destroy') // 销毁旧的裁剪区域
+            .attr('src', newImgURL) // 重新设置图片路径
+            .cropper(options) // 重新初始化裁剪区域
+    });
+    // 准备数据
+    var state = "已发布";
+    $("#saveBtn").on("click", function() {
+        state = "草稿"
+    });
+    $("#form_pub").on("submit", function(e) {
+        e.preventDefault();
+        var fd = new FormData($(this)[0]);
+        fd.append("state", state);
+        $image
+            .cropper('getCroppedCanvas', { // 创建一个 Canvas 画布
+                width: 400,
+                height: 280
+            })
+            .toBlob(function(blob) { // 将 Canvas 画布上的内容，转化为文件对象
+                // 得到文件对象后，进行后续的操作
+                fd.append("cover_img", blob);
+                pubArt(fd)
+            })
+    })
+
+    function pubArt(fd) {
+        $.ajax({
+            method: "POST",
+            url: "/my/article/add",
+            data: fd,
+            //如果向服务器提交的是FormData格式的数据，必须添加以下两个配置项
+            contentType: false,
+            processData: false,
+            success: function(res) {
+                if (res.status !== 0) {
+                    layer.msg("发布文章失败！")
+                }
+                layer.msg("发布文章失败！");
+                location.href = "/article/art_list.html";
+            }
+        })
+    }
+
+    function initSelectCate() {
+        $.ajax({
+            method: "GET",
+            url: "/my/article/cates",
+            success: function(res) {
+                if (res.status !== 0) {
+                    layer.msg("获取文章类别失败！")
+                }
+                // 渲染下拉框
+                var htmlStr = template("select_cate", res);
+                $("[name=cate_id]").html(htmlStr);
+                //下拉框必须调用layui以下方法去重新渲染
+                form.render();
+            }
+        })
+    }
+})
